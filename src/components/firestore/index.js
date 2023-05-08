ee37607638695e877f1d1eaaa2030f5ee2c1eb12
Shahmeer <shahmeerrabbani61@gmail.com>
2023-05-09 02:29:31 +0500

    firestore add Data

--- src/components/firestore/index.js
+++ src/components/firestore/index.js
@@ -1,13 +1,36 @@
-import React from "react";
+import { Timestamp, addDoc, collection, doc, setDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { db } from "../screen/configRequest";
 
 
 function Firestore(){
 
+    const [data, setData] = useState("")
+
+    const handleAddData = async () => {
+
+
+        console.log("data adding -----")
+
+        const myCollection = collection(db, "New_Data")
+        var names = document.getElementById('name').value
+
+        const payload = {
+            name: names
+        }
+
+        await addDoc(myCollection,payload);
+
+        console.log("data added",myCollection,payload)
+    }
 
     return(
         <div>
 
-            <h1>Hello World</h1>
+            <h1>Create Post</h1>
+            <hr></hr>
+            <input type="text" placeholder="What's on your mind?" onChange={(e) => setData(e.target.value)} id="name"/>
+            <button onClick={handleAddData}>Post</button>
         </div>
     )
 }
